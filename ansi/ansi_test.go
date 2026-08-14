391565b1f0bf01d99ad2a4083a3784463b7b588b
wire-oss <295680624+wire-oss@users.noreply.github.com>
2026-08-14 17:04:13 +0900

    added initial code (#13)

---
+++ ansi/ansi_test.go
@@ -0,0 +1,25 @@
+package ansi
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHumanNameParser(t *testing.T) {
+	combo, err := ParseHumanName("ctrl+alt+pgup")
+	assert.NoError(t, err)
+	assert.Equal(t, "ctrl+alt+pgup", combo.HumanName)
+}
+
+func TestHumanNameParserReorder(t *testing.T) {
+	combo, err := ParseHumanName("pgup+alt+ctrl")
+	assert.NoError(t, err)
+	assert.Equal(t, "ctrl+alt+pgup", combo.HumanName)
+}
+
+func TestHumanNameParserSpaces(t *testing.T) {
+	combo, err := ParseHumanName("pgup  + alt+ctrl")
+	assert.NoError(t, err)
+	assert.Equal(t, "ctrl+alt+pgup", combo.HumanName)
+}
