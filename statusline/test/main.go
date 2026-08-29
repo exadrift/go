667cbe42603ddc5103d0be062f31cb3e6ac99f47
wire-oss <295680624+wire-oss@users.noreply.github.com>
2026-08-30 07:59:25 +0900

    statusline resets color (#20)
    Co-authored-by: wire-oss <wire-oss@noreply.github.com>

--- statusline/test/main.go
+++ statusline/test/main.go
@@ -7,13 +7,15 @@ import (
 )
 
 func main() {
-	s := statusline.NewStatusLine()
+	s := statusline.NewStatusLine(statusline.WithSpinnerStyling("\x1b[92m"))
 	defer s.Stop()
 
 	s.Status("calculating...")
-	time.Sleep(5 * time.Second)
+	time.Sleep(3 * time.Second)
 	s.Emit("1 + 1 is 2")
 	s.Status("verifying...")
+	time.Sleep(2 * time.Second)
+	s.Status("confirming...")
 	time.Sleep(5 * time.Second)
 	s.Emit("it's definitely the case")
 }
